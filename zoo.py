3d9d06768c8aa30215c26e18d371824783de440b
Liza <lizadudyk@gmail.com>
2020-04-19 23:26:29 +0300

    Added main() to existing class

--- zoo.py
+++ zoo.py
@@ -20,4 +20,13 @@ class Zoo:
     def get_is_suitable_for_kids():
         return Zoo.is_suitable_for_kids
 
+def main():
+    lviv_zoo = Zoo(4387, "Lev", 59, "Lviv")
+    kyiv_zoo = Zoo()
+    new_york_zoo = Zoo(920295, "NY zoo", 950, "New York", 2000.0, 9.37)
+    print(lviv_zoo)
+    print(kyiv_zoo)
+    print(new_york_zoo)
+
+if __name__ == "__main__": main()
 
